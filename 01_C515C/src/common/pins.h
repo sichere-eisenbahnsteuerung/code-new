bc3d047011fcc103754f3cc9777195dfa352d675
Simon Busch <morphis@gravedo.de>
2010-11-02 15:36:07 +0100

    C515C: Add pin and type definitions

---
+++ 01_C515C/src/common/pins.h
@@ -0,0 +1,16 @@
+#ifndef _PINS_H_
+#define _PINS_H_
+
+/* Pin configuration */
+sbit PIN_EMERGENCY_STOP     = P3^4;
+sbit PIN_RS232_CTS          = P3^5;
+sbit PIN_S88_PS             = P5^0;
+sbit PIN_S88_RESET          = P5^1;
+sbit PIN_S88_CLK            = P5^2;
+sbit PIN_S88_DATA           = P5^3;
+sbit PIN_AS_PORT_I2C_SDA    = P5^4;
+sbit PIN_AS_PORT_I2C_SCL    = P5^5;
+sbit PIN_RESET              = P1^0;
+
+#endif
+
---
+++ 01_C515C/src/common/types.h
@@ -0,0 +1,10 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+
+#endif
+
