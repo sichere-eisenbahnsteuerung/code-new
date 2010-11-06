8d40f4ed42c88f376aec7676abddc9b8305cd65c
Simon Busch <morphis@gravedo.de>
2010-11-06 16:11:13 +0100

    C515C: ringbuffer: Adjust for code style

--- 01_C515C/src/common/ringbuffer.c
+++ 01_C515C/src/common/ringbuffer.c
@@ -1,16 +1,19 @@
 #include <REG515C.H>
 #include "ringbuffer.h"
 
-void ringbuffer_init(ringbuffer_t *buffer) {
+void ringbuffer_init(ringbuffer_t *buffer) 
+{
 	buffer->read_position = 0;
 	buffer->write_position = 0;
 	buffer->size = 0;
 }
 
-uint8_t ringbuffer_read(ringbuffer_t *buffer) {
+uint8_t ringbuffer_read(ringbuffer_t *buffer) 
+{
 	uint8_t interrupt_state = EAL, retval;
 	
-	if(buffer->size > 0) {
+	if(buffer->size > 0) 
+	{
         retval = buffer->content[buffer->read_position];
         buffer->size--;
         buffer->read_position++;
@@ -21,10 +24,13 @@ uint8_t ringbuffer_read(ringbuffer_t *buffer) {
     return retval;
 }
 
-void ringbuffer_write(ringbuffer_t *buffer, uint8_t value) {
+void ringbuffer_write(ringbuffer_t *buffer, uint8_t value) 
+{
 	uint8_t interrupt_state = EAL;
 
-	/*if(buffer->size > 0) {
+	/*
+	if(buffer->size > 0) 
+	{
         retval = buffer->data[buffer->read_position];
         buffer->size--;
         buffer->read_position++;
@@ -32,5 +38,6 @@ void ringbuffer_write(ringbuffer_t *buffer, uint8_t value) {
 
     EAL = interrupt_state;
 
-    return retval;	  */
+    return retval;	  
+	*/
 }
