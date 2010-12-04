015b785ea12ccd90c15083020d7e25e3f96a8dcd
Hanno Fellmann <hanno@fellmannweb.de>
2010-12-04 14:12:02 +0100

    Shared Memory Ergebnisvalidierung -> RS232(XpressNet)

--- 01_C515C/modules/betriebsmittelverwaltung/betriebsmittelverwaltung.c
+++ 01_C515C/modules/betriebsmittelverwaltung/betriebsmittelverwaltung.c
@@ -1,8 +1,9 @@
 #include "types.h"
-#include "rs232_shared_memory.h"
+#include "rs232_shared_memory.h"   
+#include "xpressnet_shared_memory.h"
 
 
-/* 
+/*
  * Shared Memory RS232 
  */
 
@@ -13,3 +14,10 @@ uint8_t rs232_input_write_pos = 0;
 uint8_t rs232_output_buffer[RS232_BUFFERSIZE] = {0};
 uint8_t rs232_output_read_pos = 0;          
 uint8_t rs232_output_write_pos = 0;
+
+
+/*
+ * Shared Memory Streckenbefehle 
+ */
+
+streckenbefehl streckenbefehl_ev_xpressnet = { IDLE, 0x00 };
