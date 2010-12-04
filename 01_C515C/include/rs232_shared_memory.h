18fa10a2bfc4151c720ed7b9a8a752d9734c252a
Hanno Fellmann <hanno@fellmannweb.de>
2010-12-04 18:05:36 +0100

    Formatierung geändert

--- 01_C515C/include/rs232_shared_memory.h
+++ 01_C515C/include/rs232_shared_memory.h
@@ -1,11 +1,15 @@
+/**
+* @file rs232_shared_memory.h
+* @brief Shared Memory Deklarationen des RS232 Treiber.
+*/
 #ifndef _RS232_SHARED_MEMORY_H_
 #define _RS232_SHARED_MEMORY_H_
 
 #define RS232_BUFFERSIZE 20
 
-extern uint8_t rs232_input_buffer[RS232_BUFFERSIZE];  
-extern uint8_t rs232_input_read_pos;          
-extern uint8_t rs232_input_write_pos;            
+extern uint8_t rs232_input_buffer[RS232_BUFFERSIZE]; 
+extern uint8_t rs232_input_read_pos;    
+extern uint8_t rs232_input_write_pos; 
 
 extern uint8_t rs232_output_buffer[RS232_BUFFERSIZE];
 extern uint8_t rs232_output_read_pos;          
