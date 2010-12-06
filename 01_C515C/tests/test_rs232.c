1f36205157c1af9d52c6671b91885b4adb5b8f18
Simon Busch <morphis@gravedo.de>
2010-12-06 16:57:36 +0100

    C515C: tests: Die Tests sollten sich nun wieder kompilieren lassen

--- 01_C515C/tests/test_rs232.c
+++ 01_C515C/tests/test_rs232.c
@@ -1,25 +1,21 @@
-
 #include <test_helper.h>
 #include <rs232.h>
-
-extern char wait_for_send;
 
 /*
  * @brief Bla Blup ...
  */
 START_TEST_FUNC(test_rs232_init_check_for_correct_hardware_configuration)
 {
-	RS232_init();
+	rs232_init();
 
 	printf("test_rs232_init_check_for_correct_hardware_configuration\n");
 
 	/* Nachbedingungen prüfen */
-	assert_is_true(BD == 0, "Baudratengenerator wurde nicht eingeschaltet");
+	assert_is_true(BD == 1, "Baudratengenerator wurde nicht eingeschaltet");
 	assert_is_true(SM0 == 0 && SM1 == 1, "Falscher Modus für den Baudratengenerator");
 	assert_is_true(SRELH == 0x03 && SRELL == 0xDF, "Falsche Gewschwindigkeit");
 	assert_is_true(REN == 1 && TI == 1, "Serielle Empfang wurde nicht eingeschaltet");
 	assert_is_true(ES == 1, "Serielle Interrupts wurden nicht eingeschaltet");
-	assert_is_true(wait_for_send == 0, "Falscher Warte-Modus für die Work-Funktionalität");
 }
 END_TEST_FUNC
 
