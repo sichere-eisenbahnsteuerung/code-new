9f8f14bec5cd84106535e774be1a8e2a140f619c
Hanno Fellmann <hanno@fellmannweb.de>
2010-12-08 17:27:27 +0100

    module_documentation.h: Doxygen-Dokumentation der Module und Hauptseite

---
+++ 01_C515C/include/module_documentation.h
@@ -0,0 +1,20 @@
+/**
+
+@defgroup drivers Treiber-Schicht
+@defgroup xpressnet_rs232 XpressNet-/RS232-Treiber
+@ingroup drivers
+@defgroup s88 S88-Treiber
+@ingroup drivers
+@defgroup ssc SSC-Treiber
+@ingroup drivers
+@defgroup i2c I2C-Treiber
+@ingroup drivers
+@defgroup notaus Notaus-Treiber
+@ingroup drivers
+
+@defgroup modules_sec Sicherheits-Schicht
+
+@defgroup modules Anwendungs-Schicht
+
+@defgroup sharedmemory Shared Memory
+*/
