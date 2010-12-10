72c9f8b69706af64f636d5f364b80d0714516ae3
Hanno Fellmann <hanno@fellmannweb.de>
2010-12-10 12:24:08 +0100

    test_xpressnet.c: Unit-Test XpressNet erweitert

--- 01_C515C/tests/test_xpressnet.c
+++ 01_C515C/tests/test_xpressnet.c
@@ -20,36 +20,131 @@ static uint8_t bytes_to_read();
 static uint8_t read_byte();
 static uint8_t peek_byte();
 
-static uint8_t weiche1_geradeaus1[] = {0x52, 0x00, 0x87, 0xD5};
-static uint8_t weiche1_geradeaus2[] = {0x52, 0x00, 0x8E, 0xDC};
-static uint8_t weiche1_abbiegen1[]  = {0x52, 0x00, 0x85, 0xD7};
-static uint8_t weiche1_abbiegen2[] = {0x52, 0x00, 0x8C, 0xDE};
+// Weiche 1
+static uint8_t weiche1_geradeaus1[] = {0x52, 0x00, 0x87};
+static uint8_t weiche1_geradeaus2[] = {0x52, 0x00, 0x8E};
+static uint8_t weiche1_abbiegen1[]  = {0x52, 0x00, 0x86};
+static uint8_t weiche1_abbiegen2[] = {0x52, 0x00, 0x8F};
 
 
-/**
- * @brief Hardware-Initialisierung testen
- * @pre Kein Streckenbefehl gesetzt.
- */
-START_TEST_FUNC(test_xpressnet_weichenbefehle)
-{
-	int i = 0;
-	xpressnet_init();
+// Lok 1
+static uint8_t lok1_v0[] = {0xE4, 0x13, 0x00, 0x01, 0x00};
+static uint8_t lok1_v1[] = {0xE4, 0x13, 0x00, 0x01, 0x08};
+static uint8_t lok1_v2[] = {0xE4, 0x13, 0x00, 0x01, 0x1F};
+static uint8_t lok1_v3[] = {0xE4, 0x13, 0x00, 0x01, 0xDD};
+
+// Lok 2
+static uint8_t lok2_v0[] = {0xE4, 0x13, 0x00, 0x02, 0x00};
+static uint8_t lok2_v1[] = {0xE4, 0x13, 0x00, 0x02, 0x08};
+static uint8_t lok2_v2[] = {0xE4, 0x13, 0x00, 0x02, 0x1F};
+static uint8_t lok2_v3[] = {0xE4, 0x13, 0x00, 0x02, 0xDD};
+
 	
+/**
+ * @brief XpressNet initialisieren
+ */
+START_TEST_FUNC(test_init_xpressnet)  
+{
 	rs232_output_read_pos = rs232_output_write_pos = 0;
 	rs232_input_read_pos = rs232_input_write_pos = 0;
 	
-	assert_is_true(bytes_to_read() == 0, "RS232-Puffer ist nicht leer!");
+	xpressnet_init();
 	
+	assert_is_true(streckenbefehl_ev_xpressnet.target == IDLE, "Nach Initialisierung Streckenbefehl nicht IDLE!");
+}
 
+/**
+ * @brief Weichenbefehle für Weiche 1 testen
+ * @pre Kein Streckenbefehl gesetzt.
+ */
+START_TEST_FUNC(test_xpressnet_weiche1)
+{
+	// Weiche 1
 	streckenbefehl_ev_xpressnet.target = WEICHE1;
 	streckenbefehl_ev_xpressnet.command = 0; // Geradeaus
 	xpressnet_work();
-	testCommand("Weiche 1 geradeaus 1", weiche1_geradeaus1, 4, WEICHE1);
+	assert_is_true(testCommand("Weiche 1 geradeaus 1", weiche1_geradeaus1, 4, WEICHE1)==0, "Fehler in Befehlssequenz!");
 	xpressnet_work();
-	testCommand("Weiche 1 geradeaus 2", weiche1_geradeaus2, 4, IDLE);
+	assert_is_true(testCommand("Weiche 1 geradeaus 2", weiche1_geradeaus2, 4, IDLE)==0, "Fehler in Befehlssequenz!");
+	xpressnet_work();
+	assert_is_true(bytes_to_read() == 0, "Daten gesendet ohne gesetzten Befehl!");
+	
+	streckenbefehl_ev_xpressnet.target = WEICHE1;
+	streckenbefehl_ev_xpressnet.command = 1; // Abbiegen
+	xpressnet_work();
+	assert_is_true(testCommand("Weiche 1 abbiegen 1", weiche1_abbiegen1, 4, WEICHE1)==0, "Fehler in Befehlssequenz!");
+	xpressnet_work();
+	assert_is_true(testCommand("Weiche 1 abbiegen 2", weiche1_abbiegen2, 4, IDLE)==0, "Fehler in Befehlssequenz!");
+	xpressnet_work();
+	assert_is_true(bytes_to_read() == 0, "Daten gesendet ohne gesetzten Befehl!");
+	
+	
 }
 END_TEST_FUNC
 
+
+/**
+ * @brief Lokbefehle für Lok1 testen
+ * @pre XpressNet initialisiert
+ */
+START_TEST_FUNC(test_xpressnet_lok1)
+{
+	streckenbefehl_ev_xpressnet.target = LOK1;
+	streckenbefehl_ev_xpressnet.command = 0;
+	xpressnet_work();
+	assert_is_true(testCommand("Lok 1 Stop", lok1_v0, 6, IDLE)==0, "Fehler in Befehlssequenz!");
+	
+	streckenbefehl_ev_xpressnet.target = LOK1;
+	streckenbefehl_ev_xpressnet.command = 1;
+	xpressnet_work();
+	assert_is_true(testCommand("Lok 1 Abkoppeln", lok1_v1, 6, IDLE)==0, "Fehler in Befehlssequenz!");
+	
+	streckenbefehl_ev_xpressnet.target = LOK1;
+	streckenbefehl_ev_xpressnet.command = 2;
+	xpressnet_work();
+	assert_is_true(testCommand("Lok 1 Ankoppeln", lok1_v2, 6, IDLE)==0, "Fehler in Befehlssequenz!");
+	
+	streckenbefehl_ev_xpressnet.target = LOK1;
+	streckenbefehl_ev_xpressnet.command = 3;
+	xpressnet_work();
+	assert_is_true(testCommand("Lok 1 Fahrt", lok1_v3, 6, IDLE)==0, "Fehler in Befehlssequenz!");
+	xpressnet_work();
+	assert_is_true(bytes_to_read() == 0, "Daten gesendet ohne gesetzten Befehl!");
+	
+}
+END_TEST_FUNC
+
+
+/**
+ * @brief Lokbefehle für Lok1 testen
+ * @pre XpressNet initialisiert
+ */
+START_TEST_FUNC(test_xpressnet_lok2)
+{
+	streckenbefehl_ev_xpressnet.target = LOK2;
+	streckenbefehl_ev_xpressnet.command = 0;
+	xpressnet_work();
+	assert_is_true(testCommand("Lok 2 Stop", lok2_v0, 6, IDLE)==0, "Fehler in Befehlssequenz!");
+	
+	streckenbefehl_ev_xpressnet.target = LOK2;
+	streckenbefehl_ev_xpressnet.command = 1;
+	xpressnet_work();
+	assert_is_true(testCommand("Lok 2 Abkoppeln", lok2_v1, 6, IDLE)==0, "Fehler in Befehlssequenz!");
+	
+	streckenbefehl_ev_xpressnet.target = LOK2;
+	streckenbefehl_ev_xpressnet.command = 2;
+	xpressnet_work();
+	assert_is_true(testCommand("Lok 2 Ankoppeln", lok2_v2, 6, IDLE)==0, "Fehler in Befehlssequenz!");
+	
+	streckenbefehl_ev_xpressnet.target = LOK2;
+	streckenbefehl_ev_xpressnet.command = 3;
+	xpressnet_work();
+	assert_is_true(testCommand("Lok 2 Fahrt", lok2_v3, 6, IDLE)==0, "Fehler in Befehlssequenz!");
+	xpressnet_work();
+	assert_is_true(bytes_to_read() == 0, "Daten gesendet ohne gesetzten Befehl!");
+	
+}
+END_TEST_FUNC
 
 /**
  * @brief Erstellt die Test Suite
@@ -59,8 +154,11 @@ END_TEST_FUNC
 ctest_suite *test_xpressnet_create_suite(void)
 {
     ctest_suite *suite = ctest_suite_create("XpressNet");
-
-    ctest_suite_add_test_func(suite, test_xpressnet_weichenbefehle);
+	
+    ctest_suite_add_test_func(suite, test_init_xpressnet);
+    ctest_suite_add_test_func(suite, test_xpressnet_weiche1);
+    ctest_suite_add_test_func(suite, test_xpressnet_lok1);
+    ctest_suite_add_test_func(suite, test_xpressnet_lok2);
 
     return suite;
 }
@@ -69,13 +167,20 @@ ctest_suite *test_xpressnet_create_suite(void)
 /** @brief Testet die Bytes eines Befehls */
 int testCommand(char *description, uint8_t *command, int command_length, streckenbefehl_ziel result_target) {
 	char problem[200];
-	uint8_t i = 0, input;
-	assert_is_true(bytes_to_read() == command_length, "Falsche Befehlslänge Weichenbefehl!");
-	for(i=0;i<command_length;i++) {
+	uint8_t i = 0, input, xor = 0;
+	
+	sprintf(problem, "Falsche Befehlslänge (%i statt %i) - %s", bytes_to_read(), command_length, description);
+	assert_is_true(bytes_to_read() == command_length, problem);
+	
+	for(i=0;i<command_length-1;i++) {
 		input = read_byte();
 		sprintf(problem, "Fehler Byte %i (0x%X statt 0x%X) - %s", i, input, command[i], description);
+		xor ^= command[i];
 		assert_is_true(input == command[i], problem);
 	}
+	input = read_byte();
+	sprintf(problem, "Fehler XOR-Byte (0x%X statt 0x%X) - %s", input, xor, description);
+	assert_is_true(input == xor, problem);
 	xpressnet_work();
 	assert_is_true(bytes_to_read() == 0, "Befehl gesendet ohne Bestätigung!");
 	write_byte(0x01);
