1f9d5176b67af303d463139afcc8d9827332a61a
Hanno Fellmann <hanno@fellmannweb.de>
2010-12-15 13:55:45 +0100

    main.c: Angepasst an den RS232-Stub.

--- 01_C515C/common/main.c
+++ 01_C515C/common/main.c
@@ -1,11 +1,12 @@
-                       
-#include <intrins.h>
+                    
 #include "main.h"
-#include "util.h"
+#include "util.h"   
+#include "c515c.h"
 
 #include "xpressnet.h"	 
 #include "rs232.h"
-
+#include "stdio.h"
+#include "rs232_win_linux.h"
 /*
  * @brief Initialize common stuff for the main program
  */
@@ -25,24 +26,28 @@ void main()
 
 	init_common();
 
-	streckenbefehl_ev_xpressnet.target = LOK2;
+	streckenbefehl_ev_xpressnet.target = LOK_2;
 	streckenbefehl_ev_xpressnet.command = 3;
-
+    
     while(1) {
         xpressnet_work();	 
     	rs232_work();
 		if(streckenbefehl_ev_xpressnet.target == IDLE) {
 			if(inp == 0) {
 				inp++;	  
-				streckenbefehl_ev_xpressnet.target = ENTKUPPLER1;
+				streckenbefehl_ev_xpressnet.target = ENTKUPPLER_E1;
 				streckenbefehl_ev_xpressnet.command = 1;
 			}		
 			else if(inp == 1) {
 				inp++;	  		 
-				streckenbefehl_ev_xpressnet.target = WEICHE1;
+				streckenbefehl_ev_xpressnet.target = WEICHE_A;
 				streckenbefehl_ev_xpressnet.command = 0;
 			}
+            else {
+                break;
+            }
 		}
 
     }
+    CloseComport(0);
 }
