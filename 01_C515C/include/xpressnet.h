515ebbf9b6593330e408570d3dfa977422baf6f6
Hanno Fellmann <hanno@fellmannweb.de>
2010-12-15 01:12:32 +0100

    xpressnet.h: Doxygen-Dokumentation entfernt.

--- 01_C515C/include/xpressnet.h
+++ 01_C515C/include/xpressnet.h
@@ -3,10 +3,12 @@
  * @ingroup xpressnet_rs232
  * @brief XpressNet-Treiber
  *
- * Sendet XpressNet-Befehle aus dem Shared Memory in der Work-Funktion und wertet die Antwort aus. Dabei wird der RS232-Treiber genutzt und selbststaendig  aufgerufen.
+ * Sendet XpressNet-Befehle aus dem Shared Memory in der Work-Funktion und 
+ * wertet die Antworten aus.
  *
  * @author Hanno Fellmann
  * @date 08.12.2010
+ * @sa xpressnet.c
  */
 
 #ifndef _XPRESSNET_H_
@@ -14,20 +16,9 @@
  
 #include "types.h"
 #include "xpressnet_shared_memory.h"
-  
-/**
- * @brief XpressNet Initialisierung
- *
- * Initialisiert XpressNet.
- * 
- * @pre RS232 muss bereits initialisiert sein.
- */
+
 void xpressnet_init ();
-
-/**
- * @brief XpressNet Work-Funktion
- */       
+   
 void xpressnet_work();
-
 
 #endif
