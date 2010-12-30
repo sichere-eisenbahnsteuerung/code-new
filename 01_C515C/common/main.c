6e81639bf1cb9eaca1a93fb87e35b5eb9e8bd409
Hanno Fellmann <hanno@fellmannweb.de>
2010-12-30 02:02:36 +0100

    main.c: Aufruf der Betriebsmittelverwaltung

--- 01_C515C/common/main.c
+++ 01_C515C/common/main.c
@@ -1,29 +1,14 @@
                        
-#include <intrins.h>
 #include "main.h"
-#include "util.h"
+#include "betriebsmittelverwaltung.h"
 
-#include "xpressnet.h"	 
-#include "rs232.h"
-
-/*
- * @brief Initialize common stuff for the main program
- */
-void init_common(void)
-{
-	enable_interrupts(1);		 
-    rs232_init();
-    xpressnet_init();
-}
 
 /*
  * @brief The method which gets called initialy by the processor
  */
 void main()
 {
-    uint8_t inp=0;
-
-	init_common();
-
-	// TODO: Betriebsmittelverwaltung aufrufen
+	betriebsmittelverwaltung_init();
+	
+	betriebsmittelverwaltung_work();
 }
