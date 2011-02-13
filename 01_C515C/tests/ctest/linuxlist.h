dc3d219a3c0be201cda503747adbb9a72af58c18
Simon Busch <morphis@gravedo.de>
2011-02-13 13:23:57 +0100

    C515C: tests: Remove copyright terms and put in the terms of the linux kernel source
    The linuxlist.h file comes from the Linux kernel source and is not copyright by the
    Hochschule Bremen !!!

--- 01_C515C/tests/ctest/linuxlist.h
+++ 01_C515C/tests/ctest/linuxlist.h
@@ -1,19 +1,8 @@
+
 /*
- *    linuxlist
- *    Copyright (C) 2011 Hochschule Bremen
- *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * The code below is part of the Linux Kernel and licensed under the terms of the GPL v2
+ * license. See the COPYING file of the Linux Kernel sources.
+ * This file is found in the linux kernel source tree at include/linux/list.h
  */
 
 #ifndef _LINUX_LLIST_H
